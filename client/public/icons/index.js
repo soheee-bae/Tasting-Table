96eba2d140e26eb5c7e2d73eb49d84ebb4d68c58
sohee28 <amybae28@gmail.com>
2023-03-11 13:00:31 -0900

    set theme colors

---
+++ client/public/icons/index.js
@@ -0,0 +1,2 @@
+export { default as Search } from './search.svg';
+export { default as User } from './user.svg';
--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
+import Home from './pages/Home/home';
 
 function App() {
   return (
-    <Routes>
-      {/* <Route index element={}/> */}
-      <Route path={'/login'} element={<div></div>} />
-    </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path={'/login'} element={<div>Login</div>} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
