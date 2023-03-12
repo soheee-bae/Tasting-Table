54997b412ea1703d423cdd1d739ef79004e4f715
sohee28 <amybae28@gmail.com>
2023-03-12 14:30:11 -0800

    added prettier in server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -10,18 +10,22 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, () => console.log(`server started on port : ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port : ${PORT}`));
 
-app.use(express.json())
+app.use(express.json());
 
 // connect to mongodb
-mongoose.connect(process.env.MDB_CONNECT, {
+mongoose.connect(
+  process.env.MDB_CONNECT,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology:true,
-}, (err) => {
-    if (err) return console.error(err)
-    console.log('Connected to mongodb')
-}) 
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to mongodb");
+  }
+);
 
-//set up routes 
-app.use('/auth', require('./routers/userRouter'))
+//set up routes
+app.use("/auth", require("./routers/userRouter"));
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,110 +1,116 @@
-const router = require('express').Router();
-const User = require('../models/userModel')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-
+const router = require("express").Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // register
-router.post("/", async(req, res) => {
-    try {
-        const { email, password, passwordVerify } = req.body;
+router.post("/", async (req, res) => {
+  try {
+    const { email, password, passwordVerify } = req.body;
 
-
-        // validation 
-        if (!email || !password || !passwordVerify) {
-            return res.status(400).json({ errorMessage: 'Please enter all required fields.' })
-        }
-
-        if (password.length < 6) {
-            return res.status(400).json({ errorMessage: 'Please enter a password of at least 6 characters.' })
-        }
-
-        if (password !== passwordVerify) {
-            return res.status(400).json({ errorMessage: 'Please enter the same password twice.' })
-        }
-
-        // Check duplicate account 
-        const existingUser = await User.findOne({ email })
-        if (existingUser) {
-            return res.status(400).json({ errorMessage: 'An account with this email already exists.' })
-        }
-
-        // Hash the password 
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
-
-        // Save a new user account to the db 
-        const newUser = new User({
-            email, passwordHash
-        })
-        const savedUser = await newUser.save();
-
-        // Log the user in 
-        const token = jwt.sign({ user: savedUser._id }, process.env.JWT_SECRET )
-        
-
-        // send the token in a HTTP-only cookie
-        res.cookie("token", token, {
-            httpOnly: true,
-        }).send();
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).send()
+    // validation
+    if (!email || !password || !passwordVerify) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please enter all required fields." });
     }
 
-})
+    if (password.length < 6) {
+      return res.status(400).json({
+        errorMessage: "Please enter a password of at least 6 characters.",
+      });
+    }
 
+    if (password !== passwordVerify) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please enter the same password twice." });
+    }
+
+    // Check duplicate account
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ errorMessage: "An account with this email already exists." });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Save a new user account to the db
+    const newUser = new User({
+      email,
+      passwordHash,
+    });
+    const savedUser = await newUser.save();
+
+    // Log the user in
+    const token = jwt.sign({ user: savedUser._id }, process.env.JWT_SECRET);
+
+    // send the token in a HTTP-only cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
 
 // login
 
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        //validate 
-        if (!email || !password) {
-            return res.status(400).json({ errorMessage: 'Please enter all required fields.' })
-        }
-
-        const existingUser = await User.findOne({ email })
-        if (!existingUser) {
-            return res.status(401).json({ errorMessage: 'Wrong email or password.' })
-        }
-
-        const checkPassword = await bcrypt.compare(password, existingUser.passwordHash)
-        if (!checkPassword) {
-            return res.status(401).json({ errorMessage: 'Wrong email or password.' })
-        }
-
-         // Log the user in 
-         const token = jwt.sign({ user: existingUser._id }, process.env.JWT_SECRET )
-        
-
-         // send the token in a HTTP-only cookie
-         res.cookie("token", token, {
-             httpOnly: true,
-         }).send();
-
-        
-  
-        
-    }catch (err) {
-        console.error(err);
-        res.status(500).send()
+    //validate
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please enter all required fields." });
     }
 
-})
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(401).json({ errorMessage: "Wrong email or password." });
+    }
 
+    const checkPassword = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+    if (!checkPassword) {
+      return res.status(401).json({ errorMessage: "Wrong email or password." });
+    }
 
-// Logout 
-     router.get('/logout', (req, res) => {
-        //clear cookie
-        res.cookie('token', "", {
-            httpOnly: true,
-            expires: new Date(0)
-        }).send()
+    // Log the user in
+    const token = jwt.sign({ user: existingUser._id }, process.env.JWT_SECRET);
+
+    // send the token in a HTTP-only cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  //clear cookie
+  res
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
     })
+    .send();
+});
 
 module.exports = router;
